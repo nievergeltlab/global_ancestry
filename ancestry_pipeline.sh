876d6669ac86a72ee8ed8624b4e648e9103e55da
Adam X. Maihofer <amaihofer@ucsd.edu>
2020-03-23 09:50:20 -0700

    Update ancestry_pipeline.sh

--- ancestry_pipeline.sh
+++ ancestry_pipeline.sh
@@ -26,20 +26,44 @@
 #User: Location of convertf tool from EIGENSOFT (Download from http://www.hsph.harvard.edu/alkes-price/software/)
  eigensoft_loc="$WORKING_DIR"/EIG5.0.2/bin/convertf
  
-#Name of the list of ancestry panel SNP rsids (packaged with script, be sure that this is in WORKING_DIR)
+#User: Name of the list of ancestry panel SNP rsids (packaged with script, be sure that this is in WORKING_DIR)
  snpweights_snplist=hgdp_kgp_merged_v3_jointsample_v4_k6_bb.snplist
  
-#Name of ancestry panel (packaged with script, be sure that this is in WORKING_DIR)
+#User: Name of ancestry panel (packaged with script, be sure that this is in WORKING_DIR)
  snpweightfile_path=hgdp_kgp_merged_v3_jointsample_v4_k6_bb.snpweightrefpanel
  
-#Name of ancestry panel cluster centers (packaged with script, be sure that this is in WORKING_DIR)
+#User: Name of ancestry panel cluster centers (packaged with script, be sure that this is in WORKING_DIR)
  snpweight_clustercenters=hgdp_kgp_merged_v3_jointsample_v4_k6_bb.snpweightrefpanel_clustercenters.csv
 
-
-#Give path of Manufacturer SNP ID to rs-id conversion table.Illumina/Affy provide these 
+#User: Name of panel chosen ('Default': works for most illumina. AffyBB: Designed for Affy biobank chip. GSA: Designed for Illumina GSA)
+ panel=Default
+ 
+ if [ $panel == "Default" ]
+ then
+  snpweights_snplist=hgdp_kgp_merged_v3_jointsample_v4_k6.snplist
+  snpweightfile_path=hgdp_kgp_merged_v3_jointsample_v4_k6.snpweightrefpanel
+  snpweight_clustercenters=hgdp_kgp_merged_v3_jointsample_v4_k6.snpweightrefpanel_clustercenters.csv
+ fi
+ 
+  if [ $panel == "GSA" ]
+ then
+  snpweights_snplist=hgdp_kgp_merged_v3_jointsample_v4_k6.snplist
+  snpweightfile_path=hgdp_kgp_merged_gsa_v3_jointsample_v4_k6.snpweightrefpanel
+  snpweight_clustercenters=hgdp_kgp_merged_gsa_v3_jointsample_v4_k6_forsnpweights.snpweightrefpanel_clustercenters.csv
+ fi
+ 
+ if [ $panel == "AffyBB" ]
+ then
+  snpweights_snplist=hgdp_kgp_merged_v3_jointsample_v4_k6_bb.snplist
+  snpweightfile_path=hgdp_kgp_merged_v3_jointsample_v4_k6_bb.snpweightrefpanel
+  snpweight_clustercenters=hgdp_kgp_merged_v3_jointsample_v4_k6_bb.snpweightrefpanel_clustercenters.csv
+ fi
+ 
+#Give path of Manufacturer SNP ID to rs-id conversion table.
+#Illumina/Affy provide these 
 #UK Biobank Array snplist already provided
 
-#For Illumina: Go to the kit support page for the array, click downloads, then click ArrayNameHere support files
+#For Illumina panels: Go to the kit support page for the array, click downloads, then click ArrayNameHere support files
 #the file should be under the name of "ArrayNameHere Loci Name to rsID Conversion File"
 
  illumina_snplist=ukb_affy_to_snp.txt
@@ -52,6 +76,9 @@
  rsidfile="$WORKING_DIR"/"$illumina_snplist"_nodot_first
 
 ## Call subject ancestries (1000 subjects are processed at a time enhance built in parallelization)
+#Note: This is programmed to run on a local node. Generally this is not a problem even for hundreds of thousands of subjects.
+#If you MUST run this as a job script, then edit the call_ancestry_v2_may6_2016.sh file,
+#replacing the final 'bash' command with your job submission command.
  chmod u+rwx scripts/call_ancestry_v2_may6_2016.sh
  bash scripts/call_ancestry_v2_may6_2016.sh $plink_location $bfile $rsidfile $bfile_directory $snpweights_snplist $snpweights_path $snpweightfile_path $eigensoft_loc
 
@@ -60,7 +87,7 @@
 
 #Call into the ancestry folder, classify subject ancestry, produce an ancestry PC plot
  cd "$WORKING_DIR"/ancestry 
- Rscript --vanilla "$WORKING_DIR"/scripts/ancestry_plots_v4_mar1_2017.R "$bfile".predpc_oneweek ../"$snpweight_clustercenters"
+ Rscript --vanilla "$WORKING_DIR"/scripts/ancestry_plots_v4_mar1_2017.R "$bfile".predpc_oneweek ../"$snpweight_clustercenters" $panel
 
  
 
